7d6f421ec69ba4d3ee331dd75c694764e988de8f
TheLooped <95268961+TheLooped@users.noreply.github.com>
2023-10-15 18:32:29 -0500

    Feat: Full Raw Mode
    Full raw mode, byte by byte input reading

--- main.c
+++ main.c
@@ -1,15 +1,33 @@
+/*** Includes ***/
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+/*** Data ***/
+
 struct termios orig_termios;
 
-void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
+/*** Terminal ***/
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode() { /*{{{*/
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  die("tcsetattr");
+} /*}}}*/
 
 void enableRawMode() {
 
   // This edits a terminals attributes using orig_termios
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &orig_termios))
+    die("tcgetattr");
 
   // calls Disable raw at exit
   atexit(disableRawMode);
@@ -17,21 +35,36 @@ void enableRawMode() {
   // initalized a struct called raw that takes attributes from orig_termios
   struct termios raw = orig_termios;
 
+  // Disable some terminal defaults
   // bit shift raw so no 'ECHO' is shown
-  raw.c_lflag &= ~(ECHO);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_cflag |= (CS8);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
   // Set the attributes and flush
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
+/*** Init ***/
+
 int main() {
   enableRawMode();
 
-  char c;
-
   // Reading from stdin And quits upon recieving 'q'
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  while (1) {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+      die("read");
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q')
+      break;
   }
-
   return 0;
 }
