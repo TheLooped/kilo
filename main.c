ef01641c517871fb02d73831ec47236e91857596
TheLooped <95268961+TheLooped@users.noreply.github.com>
2023-10-15 17:52:52 -0500

    Feat: RawMode

--- main.c
+++ main.c
@@ -1,1 +1,37 @@
-int main() { return 0; }
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
+
+void enableRawMode() {
+
+  // This edits a terminals attributes using orig_termios
+  tcgetattr(STDIN_FILENO, &orig_termios);
+
+  // calls Disable raw at exit
+  atexit(disableRawMode);
+
+  // initalized a struct called raw that takes attributes from orig_termios
+  struct termios raw = orig_termios;
+
+  // bit shift raw so no 'ECHO' is shown
+  raw.c_lflag &= ~(ECHO);
+
+  // Set the attributes and flush
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+  enableRawMode();
+
+  char c;
+
+  // Reading from stdin And quits upon recieving 'q'
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+  }
+
+  return 0;
+}
